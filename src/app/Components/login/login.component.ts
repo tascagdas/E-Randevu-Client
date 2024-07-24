da525d2a87035f422747ebd600ad0c6f081df9f1
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-07-24 13:28:36 +0300

    form-validate-angular kutuphanesi ile basit bir validsayon hazirlandi. Login model olusturuldu

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,14 +1,19 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { LucideAngularModule } from 'lucide-angular';
+import { LoginModel } from '../../models/login.model';
+import { FormValidateDirective } from 'form-validate-angular';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, FormsModule, FormValidateDirective],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+login:LoginModel = new LoginModel()
+
   @ViewChild('passwordInput') passwordInput: ElementRef<HTMLInputElement> | undefined 
   handleShowPassword() {
     if (this.passwordInput == undefined) return;
