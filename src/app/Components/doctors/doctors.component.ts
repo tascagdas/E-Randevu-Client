b9af3067869226ef767f143c45762797440dbc6e
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-07-26 13:53:55 +0300

    doktor sayfasinda doktorlari api'dan cekme ve dongude donme islemleri

--- src/app/Components/doctors/doctors.component.ts
+++ src/app/Components/doctors/doctors.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { LucideAngularModule } from 'lucide-angular';
+import { HttpService } from '../../services/http.service';
+import { DoctorModel } from '../../models/doctor-model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-doctors',
   standalone: true,
-  imports: [RouterLink, LucideAngularModule],
+  imports: [RouterLink, LucideAngularModule, CommonModule],
   templateUrl: './doctors.component.html',
-  styleUrl: './doctors.component.css'
+  styleUrl: './doctors.component.css',
 })
-export class DoctorsComponent {
+export class DoctorsComponent implements OnInit {
+  doctors: DoctorModel[];
 
+  constructor(private http: HttpService) { }
+
+  ngOnInit(): void {
+    this.getAllDoctors();
+  }
+  
+  getAllDoctors() {
+    this.http.post<DoctorModel[]>("doctors/getalldoctors", {}, (resp) => {
+      this.doctors=resp.data
+    })
+  }
 }
