53f962a38ba2d350df8803ac2bd43085ae2b9086
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-07-28 17:24:42 +0300

    doktor ekleme işlemleri tamam test edildi.

--- src/app/Components/doctors/doctors.component.ts
+++ src/app/Components/doctors/doctors.component.ts
@@ -1,29 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { LucideAngularModule } from 'lucide-angular';
 import { HttpService } from '../../services/http.service';
 import { DoctorModel } from '../../models/doctor-model';
 import { CommonModule } from '@angular/common';
+import { departments } from '../../Constans/constans';
+import { FormsModule, NgForm } from '@angular/forms';
+import { FormValidateDirective } from 'form-validate-angular';
 
 @Component({
   selector: 'app-doctors',
   standalone: true,
-  imports: [RouterLink, LucideAngularModule, CommonModule],
+  imports: [
+    RouterLink,
+    LucideAngularModule,
+    CommonModule,
+    FormsModule,
+    FormValidateDirective,
+  ],
   templateUrl: './doctors.component.html',
   styleUrl: './doctors.component.css',
 })
 export class DoctorsComponent implements OnInit {
   doctors: DoctorModel[];
 
-  constructor(private http: HttpService) { }
+  departments = departments;
+
+  @ViewChild('addModalCloseButton') addModalCloseButton:ElementRef<HTMLButtonElement> | undefined
+  createDoctorModel: DoctorModel = new DoctorModel();
+
+  constructor(private http: HttpService) {}
 
   ngOnInit(): void {
     this.getAllDoctors();
   }
-  
+
   getAllDoctors() {
-    this.http.post<DoctorModel[]>("doctors/getalldoctors", {}, (resp) => {
-      this.doctors=resp.data
-    })
+    this.http.post<DoctorModel[]>('doctors/getalldoctors', {}, (resp) => {
+      this.doctors = resp.data;
+
+    });
+  }
+
+  add(form: NgForm) {
+    if (form.valid) {
+      this.http.post<string>('Doctors/CreateDoctor',this.createDoctorModel,(resp) => {
+          this.getAllDoctors();
+          this.addModalCloseButton.nativeElement.click()
+          this.createDoctorModel = new DoctorModel();
+        }
+      );
+    }
   }
 }
--- src/app/Constans/constans.ts
+++ src/app/Constans/constans.ts
@@ -1,1 +1,74 @@
+import { DepartmentModel } from '../models/department.model';
+
 export const api: string = 'https://localhost:7053/api/';
+
+export const departments: DepartmentModel[] = [
+  {
+    name: 'Doktor',
+    value: 0,
+  },
+  {
+    name: 'Acil',
+    value: 1,
+  },
+  {
+    name: 'Radyoloji',
+    value: 2,
+  },
+  {
+    name: 'Kardioloji',
+    value: 3,
+  },
+  {
+    name: 'Dermatoloji',
+    value: 4,
+  },
+  {
+    name: 'Endokrinoloji',
+    value: 5,
+  },
+  {
+    name: 'Gastroenteroloji',
+    value: 6,
+  },
+  {
+    name: 'Genel Cerrahi',
+    value: 7,
+  },
+  {
+    name: 'Jinekoloji ve Obstetrik',
+    value: 8,
+  },
+  {
+    name: 'Hematoloji',
+    value: 9,
+  },
+  {
+    name: 'Enfeksiyon Hastalıkları',
+    value: 10,
+  },
+  {
+    name: 'Nefroloji',
+    value: 11,
+  },
+  {
+    name: 'Nöroloji',
+    value: 12,
+  },
+  {
+    name: 'Ortopedi',
+    value: 13,
+  },
+  {
+    name: 'Pediatri',
+    value: 14,
+  },
+  {
+    name: 'Psikiyatri',
+    value: 15,
+  },
+  {
+    name: 'Pulmonoloji',
+    value: 16,
+  },
+];
