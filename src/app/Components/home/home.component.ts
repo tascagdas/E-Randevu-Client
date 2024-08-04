1174648e0c67aa25630461d4be4a41ef2efd0951
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-08-04 23:38:40 +0300

    departman seçimi ve doktor seçimi için ön hazırlık Tablo ile randevuları göstermek için.

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { departments } from '../../Constans/constans';
+import { DoctorModel } from '../../models/doctor-model';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  departments = departments;
+  doctors: DoctorModel[] = [];
+  selectedDepartmentValue: number = -1;
+  selectedDoctorId:string
 }
