3091ab28c549c53b5839db68b8f692aeb6bace67
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-08-05 18:43:08 +0300

    takvim türkçeye çevrildi.

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -6,6 +6,7 @@ import { CommonModule } from '@angular/common';
 import { DxSchedulerModule } from 'devextreme-angular';
 import { AppointmentService } from '../../services/appointment.service';
 import { Appointment } from '../../models/appointment.model';
+import { locale } from 'devextreme/localization';
 
 @Component({
   selector: 'app-home',
@@ -19,8 +20,27 @@ export class HomeComponent {
   currentDate: Date = new Date(Date.now());
 
   constructor(appoService: AppointmentService) {
+    locale('tr');
     this.dummyAppointments = appoService.getAppointments();
   }
+
+  appointments: any = [
+    {
+      startDate: new Date('08.05.2024 08:45'),
+      endDate: new Date('08.05.2024 09:45'),
+      title: 'Install New Database',
+    },
+    {
+      startDate: new Date('08.05.2024 11:45'),
+      endDate: new Date('08.05.2024 12:45'),
+      title: 'UnInstall New Database',
+    },
+    {
+      startDate: new Date('08.06.2024 14:45'),
+      endDate: new Date('08.06.2024 15:45'),
+      title: 'Setup Database',
+    }
+  ];
 
   departments = departments;
   doctors: DoctorModel[] = [];
