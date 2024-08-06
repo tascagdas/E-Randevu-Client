d73338ca04b19584dd8a34906853ac6e8aab31b3
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-08-06 17:15:54 +0300

    getDoctors ve getappointments tamam. Doktorlar seçilen departmana göre listelenip doktora göre randevulara istek atılıyor.

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -4,9 +4,10 @@ import { DoctorModel } from '../../models/doctor-model';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { DxSchedulerModule } from 'devextreme-angular';
-import { AppointmentService } from '../../services/appointment.service';
-import { Appointment } from '../../models/appointment.model';
 import { locale } from 'devextreme/localization';
+import { HttpService } from '../../services/http.service';
+import { AppointmentModel } from '../../models/appointment.model';
+
 
 @Component({
   selector: 'app-home',
@@ -16,34 +17,39 @@ import { locale } from 'devextreme/localization';
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  dummyAppointments: Appointment[];
   currentDate: Date = new Date(Date.now());
 
-  constructor(appoService: AppointmentService) {
+  constructor(private http: HttpService) {
     locale('tr');
-    this.dummyAppointments = appoService.getAppointments();
   }
-
-  appointments: any = [
-    {
-      startDate: new Date('08.05.2024 08:45'),
-      endDate: new Date('08.05.2024 09:45'),
-      title: 'Install New Database',
-    },
-    {
-      startDate: new Date('08.05.2024 11:45'),
-      endDate: new Date('08.05.2024 12:45'),
-      title: 'UnInstall New Database',
-    },
-    {
-      startDate: new Date('08.06.2024 14:45'),
-      endDate: new Date('08.06.2024 15:45'),
-      title: 'Setup Database',
-    }
-  ];
 
   departments = departments;
   doctors: DoctorModel[] = [];
   selectedDepartmentValue: number = -1;
   selectedDoctorId: string;
+
+  appointments: AppointmentModel[] = [];
+  getDoctors() {
+    this.selectedDoctorId = '';
+    this.http.post<DoctorModel[]>(
+      'Appointments/GetDoctorsByDepartment',
+      { DepartmentValue: this.selectedDepartmentValue },
+      (resp) => {
+        this.doctors = resp.data;
+      }
+    );
+  }
+
+  getAppointments() {
+    if (this.selectedDoctorId) {
+          this.http.post<AppointmentModel[]>(
+            'Appointments/GetAppointmentsByDoctorId',
+            { DoctorId: this.selectedDoctorId },
+            (resp) => {
+              this.appointments = resp.data;
+            }
+          );
+    }
+
+  }
 }
--- src/app/models/appointment.model.ts
+++ src/app/models/appointment.model.ts
@@ -1,7 +1,11 @@
-export class Appointment {
+import { PatientModel } from "./patient.model";
+
+export class AppointmentModel {
+  id: string;
   title: string;
   startDate: Date;
   endDate: Date;
+  patient: PatientModel;
   dayLong?: boolean;
   recurrence?: string;
 }
